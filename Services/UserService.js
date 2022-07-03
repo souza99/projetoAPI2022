e34e5f60984384f800264cdded31c828dd777315
souza99 <jpfsouza99@gmail.com>
2022-07-03 20:04:30 -0300

    Api com usuario

---
+++ Services/UserService.js
@@ -0,0 +1,11 @@
+var users = [];
+module.exports = class UserService {
+    static getUsers() {
+        return { users: users }
+    }
+    static save(user) {
+        users.push(user)
+        return { user: user }
+    }
+   
+}
