c7855083e6b5188d923bc95e29559e8eab5b6e68
souza99 <jpfsouza99@gmail.com>
2022-08-29 20:13:25 -0300

    Config

---
+++ config.js
@@ -0,0 +1,18 @@
+"use strict"
+
+class Config {
+    static createConfig () {
+        global.config = {
+            porta: process.env.PORT || 3000,
+            db: {
+                connectMethod: "mongodb+srv",
+                name: "",
+                url: "",
+                password: "",
+                urlParams: "?retry/Writes=true&w=majority"
+            }
+        }
+    }
+}
+
+module.exports = Config.createConfig();
